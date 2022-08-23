6cbb9466da1d37fa64ffdc50782f7add65d3e84b
kgscarroll <kgscarroll@gmail.com>
2022-08-23 11:48:58 -0600

    Add and test playRound function

--- javascript.js
+++ javascript.js
@@ -7,15 +7,44 @@ let computerSelection = choices[random];
 return computerSelection;
 }
 
-computerSelection = getComputerChoice();
 
-let playerSelection = prompt('Choose Rock, Paper, or Scissors');
-if (playerSelection.toUpperCase(playerSelection) == 'ROCK' ||playerSelection.toUpperCase() == 'SCISSORS' || playerSelection.toUpperCase() == 'PAPER') {
-    game(computerSelection, playerSelection);
-} else {
-    let playerSelection = parseInt(prompt('Try again! Choose Rock, Paper, or Scissors'));
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') ||
+    (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') ||
+    (playerSelection == 'PAPER' && computerSelection == 'ROCK')
+  ) {
+    return message = ('YOU WIN! ' + playerSelection + ' beats ' + computerSelection + '!');
+    
+    
+  } else if (playerSelection == computerSelection) {
+    return message = 'YOU TIED!'
+    
+  } else {
+    return message = ('YOU LOSE! ' + playerSelection + ' loses to ' + computerSelection + '!');
+    
+  }
 }
 
 function game() {
-    console.log('working');
+    for(let i = 0; i < 5; i++; let x=0; let y=0;) {
+        let playerSelection = prompt('Choose Rock, Paper, or Scissors').toUpperCase();
+        if ((playerSelection) == 'ROCK' ||playerSelection == 'SCISSORS' || playerSelection == 'PAPER') {
+            let message = playRound(playerSelection, computerSelection);
+            console.log(message);
+            if (message.includes("WIN")) {
+                x++;
+            }  else if (message.includes("LOSE")) {
+                y++
+            } 
+            score = x + '-' + y;
+            console.log(score);
+             
+
+    
+        } else {
+            let playerSelection = parseInt(prompt('Try again! Choose Rock, Paper, or Scissors'));
+        }
+    }
+    
 }
